52dce9519d107751f7177c0adbcc1f11dbc09289
KevinKirby616 <124018388+KevinKirby616@users.noreply.github.com>
2023-01-30 23:14:34 -0800

    Update wREX.py

--- wREX.py
+++ wREX.py
@@ -4,10 +4,25 @@ import pathlib
 
 
 class REX:
-    def __init__(self) -> None:
-        self._REXOBJ = 1
+    def __init__(self, filename: 'full path to .rex file.') -> None:
+        self.dllfile = 'REX Shared Library.dll'
+        relativepath = pathlib.Path(self.dllfile)
+        self.dllinstance = ctypes.windll.LoadLibrary(str(relativepath.resolve()))
+        print(bool(self.dllinstance.REXInitializeDLL()))
+        self._rexfile = filename
+    
+    def __str__(self) -> str:
+        return str(self.dllinstance)
 
-relativepath = pathlib.Path('REX Shared Library.dll')
-REXX = ctypes.windll.LoadLibrary(str(relativepath.resolve()))
-REXX.REXInitializeDLL()
-print(REXX)
+    def UninitializeDLL(self) -> 'unload DLL':
+        print(bool(self.dllinstance.REXUninitializeDLL()))
+
+
+shit = REX(5)
+
+print(shit.UninitializeDLL())
+
+#relativepath = pathlib.Path('REX Shared Library.dll')
+#REXX = ctypes.windll.LoadLibrary(str(relativepath.resolve()))
+#REXX.REXInitializeDLL()
+#print(REXX)
